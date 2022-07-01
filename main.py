bd61e3f0028895d60f07235a74108ce11394cf70
raz0229 <raja.zafar229@gmail.com>
2022-07-01 19:04:28 +0500

    using a package instead

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-import json
 from playsound import playsound
+from googletrans import Translator
 
 text = input("Enter text: ")
+translator = Translator()
 
 # Must end with English
 languages = {
@@ -10,16 +10,13 @@ languages = {
     'Arabic': 'ar',
     'Basque': 'eu',
     "Bengali": "bn",
-    "Portuguese (Brazil)": "pt-BR",
     "Bulgarian": "bg",
     "Catalan": "ca",
-    "Cherokee": "chr",
     "Croatian": "hr",
     "Czech": "cs",
     "Danish": "da",
     "Dutch": "nl",
     "Estonian": "et",
-    "Filipino": "fil",
     "Finnish": "fi",
     "French": "fr",
     "German": "de",
@@ -62,35 +59,13 @@ languages = {
     "English": 'en',
 }
 
-connTranslate = http.client.HTTPSConnection("google-translate1.p.rapidapi.com")
-headersTranslate = {
-    'content-type': "application/x-www-form-urlencoded",
-    'Accept-Encoding': "application/gzip",
-    'X-RapidAPI-Host': "google-translate1.p.rapidapi.com",
-    'X-RapidAPI-Key': "85632300dbmsha01f5765f1a7303p18df83jsn83e04aa1780a"
-}
-
-
-def slugify(sentence):
-    return sentence.replace(' ', '%20')
-
-
-def urlify(text, target):
-    slug = slugify(text)
-    return f"q={slug}&target={target}"
-
-
 def getTranslation(abv):
-    payloadEng = urlify(text, abv)
     try:
-        connTranslate.request("POST", "/language/translate/v2",
-                              payloadEng.encode('utf-8'), headersTranslate)
-        resEng = connTranslate.getresponse()
-        dataEng = resEng.read()
-    except http.client.HTTPException:
-        return "🤖🦇 Error translating. Try again"
+        response = translator.translate(text, dest=abv).text
+    except Exception:
+        print(f"\n🤖🦇: Error translating in {abv}")
     else:
-        return json.loads(dataEng.decode("utf-8"))["data"]["translations"][0]['translatedText']
+        return response
 
 
 if __name__ == "__main__":
